7af7d4f8a2ac050b1e946436f4db466896565a8e
Gal Topper <galt@iguaz.io>
2017-09-03 16:14:03 +0800

    Benchmark evolution.

--- src/main/scala/io/iguaz/benchmark/update/Main.scala
+++ src/main/scala/io/iguaz/benchmark/update/Main.scala
@@ -20,29 +20,40 @@ object Main {
 
     val parallelMassUpdates = sys.props.get("parallel-mass-updates").map(_.toInt).getOrElse(1)
     val maxInFlightPropName = "v3io.config.kv.update.max-in-flight"
-    val maxInFlight = sys.props.get(maxInFlightPropName).map(_.toInt)
+    val payloadSizePropName = "kv.update.payload.size"
+    val maxInFlightOption = sys.props.get(maxInFlightPropName).map(_.toInt)
+    val payloadSize = sys.props.get(payloadSizePropName).map(_.toLong)
+    val capnpFactorPropName = "capn-message-size-ratio"
+    val capnpFactor = sys.props.get(capnpFactorPropName).map(_.toInt)
 
     println(s"parallelMassUpdates = $parallelMassUpdates")
     println(s"collection = $collection")
     println(s"printPeriod = $printPeriod")
+    println(s"maxInFlightOption = $maxInFlightOption")
+    println(s"payloadSize = $payloadSize")
+    println(s"capnpFactor = $capnpFactor")
 
     val start = System.currentTimeMillis()
 
+    //    val maxInFlight = maxInFlightOption.get
+
+    //    val semaphore = new Semaphore(maxInFlight)
+
     def requestIterator() = {
-      var count = 0
       var lastCycleStart = start
-      Iterator.from(0).map { i =>
+      Iterator.from(0).map { count =>
         if (count % printPeriod == 0) {
           val cycleStart = System.currentTimeMillis()
           val secondPassed = (cycleStart - start) / 1000
           val millisSinceLastCycle = cycleStart - lastCycleStart
           val millisRate = if (count == 0) 0 else printPeriod / millisSinceLastCycle
           val secondsRate = millisRate * 1000
+          //          val numInFlight = maxInFlight - semaphore.availablePermits()
+          //          println(s"[$secondsRate/sec | $numInFlight/$maxInFlight in-flight]\t$count entries written after $secondPassed seconds...")
           println(s"[$secondsRate/sec]\t$count entries written after $secondPassed seconds...")
           lastCycleStart = cycleStart
         }
-        count += 1
-        val row = SimpleRow(i.toString, Map("index" -> i.toLong))
+        val row = SimpleRow(count.toString, Map("index" -> count.toLong))
         UpdateEntry(collection, row, OverwriteMode.REPLACE)
       }
     }
@@ -50,14 +61,21 @@ object Main {
     val props = new Properties
     props.put("container-id", "1")
 
-    //DoInContainer(props) { container =>
-    val params = Map.empty ++ maxInFlight.map(maxInFlightPropName -> _)
-    // val kvOps = KeyValueOperations(container, params)
+    val params = Map.empty ++
+      maxInFlightOption.map(maxInFlightPropName -> _) ++
+      payloadSize.map(payloadSizePropName -> _) ++
+      capnpFactor.map(capnpFactorPropName -> _)
+
     val kvOps = KeyValueOperations(ContainerID(1), params)
-    val responseFutureList = List.fill(parallelMassUpdates)(kvOps.update(requestIterator()))
+
+    //    requestIterator().foreach { req =>
+    //      semaphore.acquire()
+    //      val f = kvOps.update(req.collection, req.row, req.mode)
+    //      f.onComplete(_ => semaphore.release())
+    //    }
+
+    val responseFutureList = List.fill(parallelMassUpdates)(kvOps.updateMultiple(requestIterator()))
     val responsesFuture = Future.sequence(responseFutureList)
-    val responses = Await.result(responsesFuture, Duration.Inf)
-    //}
-    println(s"responses = $responses")
+    Await.result(responsesFuture, Duration.Inf)
   }
 }
