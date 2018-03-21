0e4e4dc19e13a8bb897aece7a7b4fbb99dc78228
Gal Topper <galt@iguaz.io>
2018-03-21 11:27:19 +0200

    Update to work with latest dev. Shorten logs.

--- src/main/scala/io/iguaz/benchmark/update/Main.scala
+++ src/main/scala/io/iguaz/benchmark/update/Main.scala
@@ -2,7 +2,6 @@ package io.iguaz.benchmark.update
 
 import java.net.URI
 
-import io.iguaz.v3io.api.container.ContainerID
 import io.iguaz.v3io.kv._
 
 object Main {
@@ -12,16 +11,13 @@ object Main {
     val collectionUri = new URI("v3io", "1", args(0), null, null)
 
     val parallelMassUpdates = sys.props.get("parallel-mass-updates").map(_.toInt).getOrElse(1)
-    val maxInFlightPropName = "v3io.config.kv.update.max-in-flight"
     val payloadSizePropName = "kv.update.payload.size"
-    val maxInFlightOption = sys.props.get(maxInFlightPropName).map(_.toInt)
     val payloadSize = sys.props.get(payloadSizePropName).map(_.toLong)
     val capnpFactorPropName = "capn-message-size-ratio"
     val capnpFactor = sys.props.get(capnpFactorPropName).map(_.toInt)
 
     println(s"parallelMassUpdates = $parallelMassUpdates")
     println(s"collectionUri = $collectionUri")
-    println(s"maxInFlightOption = $maxInFlightOption")
     println(s"payloadSize = $payloadSize")
     println(s"capnpFactor = $capnpFactor")
 
@@ -31,11 +27,10 @@ object Main {
     }
 
     val params = Map.empty ++
-      maxInFlightOption.map(maxInFlightPropName -> _) ++
       payloadSize.map(payloadSizePropName -> _) ++
       capnpFactor.map(capnpFactorPropName -> _)
 
-    val kvOps = KeyValueOperations(ContainerID(1), params)
+    val kvOps = KeyValueOperations(params)
 
     val responseIteratorList = List.fill(parallelMassUpdates)(kvOps.updateItemsIterator(requestIterator()).map(_ => ()))
     val responsesIterator = responseIteratorList.foldRight(Iterator.continually(())) {
